4a70b5215aaa67a420431bbf99ad8beb38d8d073
cinc <cinc@985fc4c0-a511-0410-a4be-e915bfa2b0ec>
2006-11-26 12:36:45 +0000

    Implemented wpRequestObjectMutex() and wpReleaseObjectMutex(). These are implemented using the OS/2 api.

--- desktop/trunk/desktop/include/desktoptypes.h
+++ desktop/trunk/desktop/include/desktoptypes.h
@@ -1,7 +1,7 @@
 #ifndef DESKTOPTYPES_H_INCLUDED
 #define DESKTOPTYPES_H_INCLUDED
 
-
+/* Inuse lists */
 typedef struct _USEITEM
 {
    gulong  type;              /* Item type */
@@ -14,9 +14,13 @@ typedef struct _MEMORYITEM
    gulong   cbBuffer;           /* Number of bytes in memory block */
 } MEMORYITEM, *PMEMORYITEM;
 
-
 #define USAGE_MEMORY          1
- 
+
+/* Misc types used with objects */
+typedef GSList *PGSList;
+typedef GMutex *PGMutex;
+
+/* Errors */ 
 #define NOMERROR_NOT_ENOUGH_MEMORY 8
   
 #endif /* DESKTOPTYPES_H_INCLUDED */
