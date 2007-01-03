e2caa0646fd9a418f4fd85dee13163dc36ac74d8
cinc <cinc@985fc4c0-a511-0410-a4be-e915bfa2b0ec>
2007-01-03 09:24:31 +0000

    Added a few #defines for menu handling.

--- desktop/include/desktoptypes.h
+++ desktop/include/desktoptypes.h
@@ -1,6 +1,17 @@
 #ifndef DESKTOPTYPES_H_INCLUDED
 #define DESKTOPTYPES_H_INCLUDED
 
+/* Menu handling */
+typedef gpointer wpMenuId;
+
+typedef struct _wpMenuItemInfo
+{
+  wpMenuId menuId;      /* A unique ID calculated by the system        */
+  gchar* pszMenuItemId; /* ID string like classname.submenu.menuitem   */
+  gchar* pszMenuText;   /* The actual menu text                        */
+  gpointer ptrUserinfo; /* Pointer to more menu item info for the user */
+}wpMenuItemInfo;
+
 /* Inuse lists */
 typedef struct _USEITEM
 {
@@ -19,6 +30,15 @@ typedef struct _MEMORYITEM
 #define OPEN_DEFAULT          0
 #define OPEN_CONTENTS         1
 #define OPEN_SETTINGS         2
+
+/* Flags for menu methods */
+#define MENU_OBJECTPOPUP           0x00000001
+#define MENU_OPENVIEWPOPUP         0x00000002
+#define MENU_TITLEBARPULLDOWN      0x00000003
+
+/* Menu IDs*/
+#define WPMENUID_OPEN              1
+#define WPMENUID_PROPERTIES        112
 
 /* Misc types used with objects */
 typedef GSList *PGSList;
